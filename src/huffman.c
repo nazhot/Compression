1eff39a55b86d0871bd55ff43f37854acd16fd48
nazhot <15216154+nazhot@users.noreply.github.com>
2024-04-04 14:38:16 -0600

    Working on Main Huffman Node Loop
    Appears to be working, creating new function to full check.

--- src/huffman.c
+++ src/huffman.c
@@ -1,7 +1,9 @@
 #include "huffman.h"
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 struct Node {
     struct Node *left;
@@ -18,8 +20,11 @@ static void printNode( const struct Node *node ) {
     printf( "     Right Child: %p\n", node->right );
 }
 
-static struct Node combineNodes( struct Node *node1, struct Node *node2 ) {
-    return ( struct Node ) { node1, node2, node1->weight + node2->weight, NULL };
+static struct Node combineAndResetNodes( struct Node *node1, struct Node *node2 ) {
+    struct Node newNode = ( struct Node ) { node1, node2, node1->weight + node2->weight, NULL };
+    node1->weight = UINT_MAX;
+    node2->weight = UINT_MAX;
+    return newNode; 
 }
 
 static int orderNodesDescending( const void *node1, const void *node2 ) {
@@ -57,7 +62,8 @@ void huffman( const char *stringToCompress, const size_t stringLength ) {
     for ( uint8_t i = 1; i != 0; ++i ) { //goes from 1 to 255
         if ( characterCounts[i] ) {
             printf( "%uc: %i\n", i, characterCounts[i] );
-            queue1[queue1Index++] = ( struct Node ) { NULL, NULL, characterCounts[i], i };
+            queue1[queue1Index] = ( struct Node ) { NULL, NULL, characterCounts[i], i };
+            queue2[queue1Index++] = ( struct Node ) { NULL, NULL, UINT_MAX, 0 };
         } else {
             *outputPointer++ = i;
             *outputPointer++ = ',';
@@ -74,8 +80,25 @@ void huffman( const char *stringToCompress, const size_t stringLength ) {
     //there will always be n - 1 steps to make it, so start by decrementing
     //the value
     while ( --numUniqueCharacters ) {
-        if ( queue2Index == 0 ) {
-             
+        bool queue1LT = queue1[0].weight < queue2[0].weight; 
+        bool queue2LT;
+        //queue2Index -= queue1LT == false; //decrement the index if queue2 element is taken
+        struct Node *p_node1 = queue1LT ? &queue1[0] : &queue2[0];
+        struct Node *p_node2;
+        if ( queue1LT ) {
+            queue2LT = queue2[0].weight < queue1[1].weight; 
+            p_node2 = queue2LT ? &queue2[0] : &queue1[1];
+            //queue2Index -= queue2LT; 
+        } else {
+            queue2LT = queue2[1].weight < queue1[0].weight;
+            p_node2 = queue2LT ?  &queue2[1] : &queue1[0];
+            //queue2Index -= queue2LT;
         }
+        //need to move all of the elements forward
+        queue2[queue2Index++] = combineAndResetNodes( p_node1, p_node2 );
+
+        qsort( queue1, numUniqueCharacters, sizeof( struct Node ), orderNodesDescending );
+        qsort( queue2, numUniqueCharacters, sizeof( struct Node ), orderNodesDescending );
     }
+    printNode( &queue2[0] );
 }
