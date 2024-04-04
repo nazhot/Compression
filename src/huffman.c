2b8020db87023aa9e43a96586723bba83ca5cba8
nazhot <15216154+nazhot@users.noreply.github.com>
2024-04-04 06:29:43 -0600

    Starting Node Work

--- src/huffman.c
+++ src/huffman.c
@@ -2,16 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct Node {
+    struct Node *left;
+    struct Node *right;
+    unsigned int weight;
+};
+
 
 void huffman( char *stringToCompress, unsigned int stringLength ) {
     unsigned int characterCounts[256] = {0};
+    unsigned int numUniqueCharacters = 0;
+
     for ( size_t i = 0; i < stringLength; ++i ) {
-        characterCounts[stringToCompress[i]]++;
+        //increment characterCounts while also checking if that was the first
+        //time that character was found
+        numUniqueCharacters += ++characterCounts[stringToCompress[i]] == 1;
     }
 
     char output[512] = {0};
     char *outputPointer = &output[0];
-    
+
+    printf( "Number of unique characters: %u\n", numUniqueCharacters ); 
     for ( int i = 1; i < 256; ++i ) {
         if ( characterCounts[i] ) {
             printf( "%c: %i\n", i, characterCounts[i] );
@@ -20,5 +31,7 @@ void huffman( char *stringToCompress, unsigned int stringLength ) {
             *outputPointer++ = ',';
         }
     }
+    --outputPointer; 
+    *outputPointer = '\0';
     printf( "No counts: %s\n", output );
 }
