bd2a3d1635d2bfada97d09e6d6b24ceb9682289c
nazhot <15216154+nazhot@users.noreply.github.com>
2024-04-05 06:57:34 -0600

    Changed Huffman Test String

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "huffman.h"
 
 int main( int argc, char *argv[] ) {
     printf( "Working\n" );
-    huffman_encode( "Tester", 6 );
+    char *string = "This is a bit of a longer string just to test how it handles something like this";
+    huffman_encode( string, strlen( string ) );
     return 0;
 }
